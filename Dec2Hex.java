8ac18f1f78a6789b257ffa67e9bd44b4ec63f9b5
Sean Quigley <SQUIGL300@caledonian.ac.uk>
2023-11-14 21:00:58 +0000

    Added input validation and error handling.

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,29 +1,36 @@
-import java.util.Scanner;
+public class Dec2Hex {
 
-// This is a new comment to test Jenkins build trigger
-
-class Dec2Hex
-{
-
-    public static int Arg1;
-
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+    public static void main(String args[]) {
+        // Check if any arguments are provided
+        if (args.length == 0) {
+            // If not, print an error message and exit with status code 1 (error)
+            System.err.println("Error: No input argument provided.");
+            System.exit(1);
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        try {
+            // Try to parse the first argument as an integer
+            int arg1 = Integer.parseInt(args[0]);
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int rem, num;
+            num = arg1;
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+        } catch (NumberFormatException e) {
+            // If a NumberFormatException is thrown, handle it by printing an error message
+            // This catches the case where the argument is not an integer
+            System.err.println("Error: Non-Integer input provided.");
+            // Exit with error code
+            // System.exit(1);    
+  }
     }
 }
