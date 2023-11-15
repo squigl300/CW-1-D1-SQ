b5457615aa27db14f515a5ecdca72296c00f0d82
squigl300 <147071163+squigl300@users.noreply.github.com>
2023-11-15 00:59:50 +0000

    Update Dec2Hex.java
    Refactored to work with test cases.
    
    The main method shouldn't contain the conversion logic because it's not easy to test. Instead, we'll move the conversion logic into a separate static method that can be called by the main method and by the tests.
    
    Now uses converToHex

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -9,28 +9,28 @@ public class Dec2Hex {
         }
 
         try {
-            // Try to parse the first argument as an integer
-            int arg1 = Integer.parseInt(args[0]);
-            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-            int rem, num;
-            num = arg1;
-            String hexadecimal = "";
-            System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-            while (num != 0) {
-                rem = num % 16;
-                hexadecimal = ch[rem] + hexadecimal;
-                num = num / 16;
-            }
-
-            System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+            // Try to parse the first argument as an integer and convert it to Hex
+            String hexValue = convertToHex(args[0]);
+            System.out.println("Hexadecimal representation is: " + hexValue);
         } catch (NumberFormatException e) {
             // If a NumberFormatException is thrown, handle it by printing an error message
             // This catches the case where the argument is not an integer
             System.err.println("Error: Non-Integer input provided.");
             // Exit with error code
             // System.exit(1);    
-  }
+        }
+    }
+
+    public static String convertToHex(String decimalInput) {
+        int num = Integer.parseInt(decimalInput);
+        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        String hexadecimal = "";
+        while (num != 0) {
+            rem = num % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            num = num / 16;
+        }
+        return hexadecimal;
     }
 }
