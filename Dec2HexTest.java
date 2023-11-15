cb549798ac021d1deb281d9b02380b6e68bfd2b8
squigl300 <147071163+squigl300@users.noreply.github.com>
2023-11-15 01:45:57 +0000

    Update Dec2HexTest.java
    Fixed interconnected error when trying to run HextoDecimal Conversion method

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -1,32 +1,21 @@
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
+/**
+ * Test class for Dec2Hex. It uses JUnit4 to run tests against the convertToHex method.
+ */
 public class Dec2HexTest {
 
-    // Test to ensure that valid input is correctly parsed and the appropriate
-    // hexadecimal value is returned.
+    /**
+     * Test for the convertToHex method. Checks if the correct hexadecimal string is returned.
+     */
     @Test
-    public void testValidInput() {
-        // The expected output for the decimal value 15 is "F".
-        // The assertEquals method checks that the actual method result matches "F".
-        assertEquals("F", Dec2Hex.convertToHex("15"));
-    }
-
-    // Test to ensure that invalid input throws a NumberFormatException.
-    // This test is expected to pass if the NumberFormatException is thrown.
-    @Test(expected = NumberFormatException.class)
-    public void testInvalidInput() {
-        // The following call should throw a NumberFormatException
-        // because "abc" is not a valid integer.
-        Dec2Hex.convertToHex("abc");
-    }
-
-    // Test to ensure that an empty string as input throws a NumberFormatException.
-    // This test is expected to pass if the NumberFormatException is thrown.
-    @Test(expected = NumberFormatException.class)
-    public void testEmptyInput() {
-        // The following call should throw a NumberFormatException
-        // because an empty string is not a valid integer.
-        Dec2Hex.convertToHex("");
+    public void testConvertToHex() {
+        // Example test case
+        int input = 15; // Decimal input
+        String expectedOutput = "F"; // Expected hexadecimal output
+        String actualOutput = Dec2Hex.convertToHex(input); // Perform the conversion using Dec2Hex method
+        assertEquals("The hexadecimal conversion of " + input + " should be " + expectedOutput, 
+                      expectedOutput, actualOutput); // Assert that the expected output matches the actual output
     }
 }
