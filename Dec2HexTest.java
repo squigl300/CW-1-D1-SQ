b629f0ee5101cd9318f01f71c22f66a7c32b43b3
squigl300 <147071163+squigl300@users.noreply.github.com>
2023-11-15 01:02:52 +0000

    Update Dec2HexTest.java
    Updated Test Case alongside updated Dec2Hex.java

--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -1,20 +1,32 @@
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Dec2HexTest {
 
+    // Test to ensure that valid input is correctly parsed and the appropriate
+    // hexadecimal value is returned.
     @Test
     public void testValidInput() {
-        // Arrange
-        String input = "15";
-        String expectedOutput = "F";
-        
-        // Act
-        String actualOutput = Dec2Hex.convertToHex(input);
-        
-        // Assert
-        Assert.assertEquals(expectedOutput, actualOutput);
+        // The expected output for the decimal value 15 is "F".
+        // The assertEquals method checks that the actual method result matches "F".
+        assertEquals("F", Dec2Hex.convertToHex("15"));
     }
-    
-    // Additional test methods will go here
+
+    // Test to ensure that invalid input throws a NumberFormatException.
+    // This test is expected to pass if the NumberFormatException is thrown.
+    @Test(expected = NumberFormatException.class)
+    public void testInvalidInput() {
+        // The following call should throw a NumberFormatException
+        // because "abc" is not a valid integer.
+        Dec2Hex.convertToHex("abc");
+    }
+
+    // Test to ensure that an empty string as input throws a NumberFormatException.
+    // This test is expected to pass if the NumberFormatException is thrown.
+    @Test(expected = NumberFormatException.class)
+    public void testEmptyInput() {
+        // The following call should throw a NumberFormatException
+        // because an empty string is not a valid integer.
+        Dec2Hex.convertToHex("");
+    }
 }
