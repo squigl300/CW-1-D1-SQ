750b6858cf96d17725a942e54a1cc9dd2c5d56c5
squigl300 <147071163+squigl300@users.noreply.github.com>
2023-11-15 01:05:34 +0000

    Update Dec2Hex.java
    includes error handling for cases where no input argument is provided or when the provided input cannot be parsed as an integer. It uses an array of characters to map the remainder of the division of the number by 16 to the corresponding hexadecimal digit. The conversion loop continues until the entire number has been processed. If any errors occur during the parsing of the integer, a NumberFormatException is caught, and an error message is printed.

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,36 +1,48 @@
+// Import the Scanner class for input handling
+import java.util.Scanner;
+
+// The Dec2Hex class contains the main method and the logic for converting
+// a decimal number to its hexadecimal representation.
 public class Dec2Hex {
 
+    // The main method is the entry point of the program.
     public static void main(String args[]) {
         // Check if any arguments are provided
         if (args.length == 0) {
-            // If not, print an error message and exit with status code 1 (error)
+            // If not, print an error message to standard error stream and exit with status code 1 (error)
             System.err.println("Error: No input argument provided.");
             System.exit(1);
         }
 
         try {
-            // Try to parse the first argument as an integer and convert it to Hex
-            String hexValue = convertToHex(args[0]);
-            System.out.println("Hexadecimal representation is: " + hexValue);
-        } catch (NumberFormatException e) {
-            // If a NumberFormatException is thrown, handle it by printing an error message
-            // This catches the case where the argument is not an integer
-            System.err.println("Error: Non-Integer input provided.");
-            // Exit with error code
-            // System.exit(1);    
-        }
-    }
+            // Try to parse the first argument as an integer
+            int arg1 = Integer.parseInt(args[0]);
+            // Array of characters representing hexadecimal digits
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            // Variable to hold the remainder of the division by 16
+            int rem;
+            // Copy of the argument to be used in the conversion process
+            int num = arg1;
+            // String to build the hexadecimal representation
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-    public static String convertToHex(String decimalInput) {
-        int num = Integer.parseInt(decimalInput);
-        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        int rem;
-        String hexadecimal = "";
-        while (num != 0) {
-            rem = num % 16;
-            hexadecimal = ch[rem] + hexadecimal;
-            num = num / 16;
+            // Loop to process the number until it's fully converted
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            // Output the hexadecimal representation
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+        } catch (NumberFormatException e) {
+            // If a NumberFormatException is thrown, print an error message
+            // This happens when the argument is not a valid integer
+            System.err.println("Error: Non-Integer input provided.");
+            // Exit with status code 1 (error)
+            System.exit(1);
         }
-        return hexadecimal;
     }
 }
