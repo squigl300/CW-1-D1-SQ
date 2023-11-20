966c41ad99b1391178000f4a6a98045bb96d8979
Sean Quigley <SQUIGL300@caledonian.ac.uk>
2023-11-20 14:26:22 +0000

    Refactor project structure into packages

---
+++ src/com/myapp/Dec2HexTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * Test class for Dec2Hex. It uses JUnit4 to run tests against the convertToHex method.
+ */
+public class Dec2HexTest {
+
+    /**
+     * Test for the convertToHex method. Checks if the correct hexadecimal string is returned.
+     */
+    @Test
+    public void testConvertToHex() {
+        // Example test case
+        int input = 15; // Decimal input
+        String expectedOutput = "F"; // Expected hexadecimal output
+        String actualOutput = Dec2Hex.convertToHex(input); // Perform the conversion using Dec2Hex method
+        assertEquals("The hexadecimal conversion of " + input + " should be " + expectedOutput, 
+                      expectedOutput, actualOutput); // Assert that the expected output matches the actual output
+    }
+}
